405e0a66e1e2e73018ec156eff8b7f826d24449f
Alona Harnyk <alyonagarnyk@gmail.com>
2023-05-25 21:20:16 +0300

    25.05

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const list = document.querySelector(".list");
+const form = document.querySelector("form");
+const button = document.querySelector(".more");
+const loader = document.querySelector(".loader");
 
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
 
-function fetchEvents(keyword) {
+let pageToFetch = 0;
+let queryToFetch = "";
+
+function fetchEvents(keyword, page) {
   const params = new URLSearchParams({
     apikey: API,
     keyword,
+    page,
+    size: 200,
   });
   return fetch(`${BASE_URL}?${params}`)
     .then((response) => {
@@ -19,16 +27,24 @@ function fetchEvents(keyword) {
     .catch((error) => console.log(error));
 }
 
-function getEvents(query) {
-  fetchEvents(query).then((data) => {
-    console.log(data)
+function getEvents(query, page) {
+  loader.classList.remove('unvisible')
+  fetchEvents(query, page).then((data) => {
+    console.log(data);
+    if (!data.page.totalElements) {
+      alert(`There are no events by keyword ${query}`);
+      return;
+    }
     const events = data._embedded.events;
     console.log(events);
-    renderEvents(events)
+    renderEvents(events);
+    if (data.page.totalPages > 1 && pageToFetch + 1 !== data.page.totalPages) {
+      button.classList.remove("unvisible");
+    }
+  }).finally(() => {
+    loader.classList.add('unvisible')
   });
 }
-
-getEvents("cat");
 
 function renderEvents(events) {
   const markup = events
@@ -37,4 +53,27 @@ function renderEvents(events) {
     })
     .join("");
   list.insertAdjacentHTML("beforeend", markup);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputValue = event.target.elements.query.value;
+  if (!inputValue.trim() || inputValue === queryToFetch) {
+    return;
+  }
+  queryToFetch = inputValue;
+  pageToFetch = 0;
+  list.innerHTML = "";
+  button.classList.add("unvisible");
+  getEvents(queryToFetch, pageToFetch);
+}
+
+button.addEventListener("click", handleLoadMore);
+
+function handleLoadMore() {
+  button.classList.add("unvisible");
+  pageToFetch += 1;
+  getEvents(queryToFetch, pageToFetch);
 }
