a00fbe3798b38ffa3af62bf8fdb837ef88010fd7
Alona Harnyk <alyonagarnyk@gmail.com>
2023-06-01 21:59:30 +0300

    01.06

--- index.js
+++ index.js
@@ -1,132 +1,232 @@
-const fetchBtn = document.querySelector(".fetch");
+// const list = document.querySelector(".list");
+// const form = document.querySelector("form");
+// const button = document.querySelector(".more");
+// const loader = document.querySelector(".loader");
+
+// const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
+// const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
+
+// let pageToFetch = 0;
+// let queryToFetch = "";
+
+// function fetchEvents(keyword, page) {
+//   const params = new URLSearchParams({
+//     apikey: API,
+//     keyword,
+//     page,
+//     size: 200,
+//   });
+//   return fetch(`${BASE_URL}?${params}`)
+//     .then((response) => {
+//       // console.log(response);
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .catch((error) => console.log(error));
+// }
+
+// function getEvents(query, page) {
+//   loader.classList.remove('unvisible')
+//   fetchEvents(query, page).then((data) => {
+//     console.log(data);
+//     if (!data.page.totalElements) {
+//       alert(`There are no events by keyword ${query}`);
+//       return;
+//     }
+//     const events = data._embedded.events;
+//     console.log(events);
+//     renderEvents(events);
+//     if (data.page.totalPages > 1 && pageToFetch + 1 !== data.page.totalPages) {
+//       button.classList.remove("unvisible");
+//     }
+//   }).finally(() => {
+//     loader.classList.add('unvisible')
+//   });
+// }
+
+// function renderEvents(events) {
+//   const markup = events
+//     .map(({ name, images }) => {
+//       return `<li><h2>${name}</h2><img src='${images[0].url}' alt='${name}' width='200'></li>`;
+//     })
+//     .join("");
+//   list.insertAdjacentHTML("beforeend", markup);
+// }
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const inputValue = event.target.elements.query.value;
+//   if (!inputValue.trim() || inputValue === queryToFetch) {
+//     return;
+//   }
+//   queryToFetch = inputValue;
+//   pageToFetch = 0;
+//   list.innerHTML = "";
+//   button.classList.add("unvisible");
+//   getEvents(queryToFetch, pageToFetch);
+// }
+
+// button.addEventListener("click", handleLoadMore);
+
+// function handleLoadMore() {
+//   button.classList.add("unvisible");
+//   pageToFetch += 1;
+//   getEvents(queryToFetch, pageToFetch);
+// }
+
+// const list = document.querySelector(".list");
+// const form = document.querySelector("form");
+// const button = document.querySelector(".more");
+// const loader = document.querySelector(".loader");
+
+// axios.defaults.baseURL = "https://app.ticketmaster.com/discovery/v2/";
+// const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
+
+// let pageToFetch = 0;
+// let queryToFetch = "";
+
+// async function fetchEvents(keyword, page) {
+//   try {
+//     const { data } = await axios("events.json", {
+//       params: {
+//         apikey: API,
+//         keyword,
+//         page,
+//         size: 200,
+//       },
+//     });
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+
+//   }
+// }
+
+// async function getEvents(query, page) {
+//   loader.classList.remove("unvisible");
+//   const data = await fetchEvents(query, page);
+//   if (!data.page.totalElements) {
+//     alert(`There are no events by keyword ${query}`);
+//     return;
+//   }
+//   const events = data._embedded.events;
+//   renderEvents(events);
+//   if (data.page.totalPages > 1 && pageToFetch + 1 !== data.page.totalPages) {
+//     button.classList.remove("unvisible");
+//   }
+//   loader.classList.add("unvisible");
+// }
+
+// function renderEvents(events) {
+//   const markup = events
+//     .map(({ name, images }) => {
+//       return `<li><h2>${name}</h2><img src='${images[0].url}' alt='${name}' width='200'></li>`;
+//     })
+//     .join("");
+//   list.insertAdjacentHTML("beforeend", markup);
+// }
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const inputValue = event.target.elements.query.value;
+//   if (!inputValue.trim() || inputValue === queryToFetch) {
+//     return;
+//   }
+//   queryToFetch = inputValue;
+//   pageToFetch = 0;
+//   list.innerHTML = "";
+//   button.classList.add("unvisible");
+//   getEvents(queryToFetch, pageToFetch);
+//   form.reset()
+// }
+
+// button.addEventListener("click", handleLoadMore);
+
+// function handleLoadMore() {
+//   button.classList.add("unvisible");
+//   pageToFetch += 1;
+//   getEvents(queryToFetch, pageToFetch);
+// }
+
 const list = document.querySelector(".list");
-const addBtn = document.querySelector(".add");
-const formWrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector("form");
 const loader = document.querySelector(".loader");
+const guard = document.querySelector(".guard");
 
-addBtn.style.display = "none";
-loader.style.display = "none";
+axios.defaults.baseURL = "https://app.ticketmaster.com/discovery/v2/";
+const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
 
-const BASE_URL = "http://localhost:8080";
+let pageToFetch = 0;
+let queryToFetch = "";
 
-fetchBtn.addEventListener("click", getUsers);
-
-function getUsers() {
-  loader.style.display = "block";
-  fetch(`${BASE_URL}/users`)
-    .then((response) => response.json())
-    .then((users) => {
-      const markup = users
-        .map(
-          ({ name, email, id }) => `
-            <li id=${id}>
-            <p>Name: <span class='name'>${name}</span></p>
-            <p>Email: <span class='email'>${email}</span></p>
-            <button class='delete' type='button'>Delete</button>
-            <button class='edit' type='button'>Edit</button>
-            </li>
-            `
-        )
-        .join("");
-      list.innerHTML = "";
-      list.insertAdjacentHTML("afterbegin", markup);
-      fetchBtn.style.display = "none";
-      addBtn.style.display = "inline";
-      const delBtns = document.querySelectorAll(".delete");
-      delBtns.forEach((btn) => btn.addEventListener("click", deleteUser));
-      const editBtns = document.querySelectorAll(".edit");
-      editBtns.forEach((btn) => btn.addEventListener("click", editUser));
-    })
-    .catch((error) => console.log(error))
-    .finally(() => {
-      loader.style.display = "none";
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        getEvents(queryToFetch, pageToFetch);
+      }
     });
-}
+  },
+  { rootMargin: "200px" }
+);
 
-function deleteUser(event) {
-  const id = event.target.parentNode.id;
-  const options = {
-    method: "DELETE",
-    headers: {
-      authorization: "admin",
-    },
-  };
-  event.target.textContent = "Deleting";
-  fetch(`${BASE_URL}/users/${id}`, options)
-    .then(() => getUsers())
-    .catch((error) => console.log(error));
-}
-
-addBtn.addEventListener("click", addUser);
-
-function createFormMarkup(name = "", email = "") {
-  return `<form>
-    <label>
-    Name:
-    <input type='text' name='name' value='${name}'>
-    </label>
-    <label>
-    Email:
-    <input type='email' name='email' value='${email}'>
-    </label>
-    <button class='save'>Save</button>
-    </form>`;
-}
-
-function addUser() {
-  formWrapper.innerHTML = createFormMarkup();
-  const form = document.querySelector("form");
-  const saveBtn = document.querySelector(".save");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const user = {
-      name: event.target.elements.name.value,
-      email: event.target.elements.email.value,
-    };
-    const options = {
-      method: "POST",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
+async function fetchEvents(keyword, page) {
+  try {
+    const { data } = await axios("events.json", {
+      params: {
+        apikey: API,
+        keyword,
+        page,
+        size: 200,
       },
-    };
-    saveBtn.textContent = "Saving..";
-    fetch(`${BASE_URL}/users`, options)
-      .then(() => {
-        getUsers();
-        formWrapper.innerHTML = "";
-      })
-      .catch((error) => console.log(error));
-  });
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function editUser(event) {
-  const id = event.target.parentNode.id;
-  const editBtn = event.target;
-  const name = event.target.parentNode.querySelector(".name").textContent;
-  const email = event.target.parentNode.querySelector(".email").textContent;
-  formWrapper.innerHTML = createFormMarkup(name, email);
-  const form = document.querySelector("form");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const user = {
-      name: event.target.elements.name.value,
-      email: event.target.elements.email.value,
-    };
-    const options = {
-      method: "PUT",
-      body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-      },
-    };
-    editBtn.textContent = "Editing";
-    fetch(`${BASE_URL}/users/${id}`, options)
-      .then(() => {
-        getUsers();
-        formWrapper.innerHTML = "";
-      })
-      .catch((error) => console.log(error))
-      .finally(() => (event.target.textContent = "Edit"));
-  });
+async function getEvents(query, page) {
+  loader.classList.remove("unvisible");
+  const data = await fetchEvents(query, page);
+  if (!data.page.totalElements) {
+    alert(`There are no events by keyword ${query}`);
+    return;
+  }
+  const events = data._embedded.events;
+  renderEvents(events);
+  loader.classList.add("unvisible");
+  pageToFetch += 1;
+  observer.observe(guard);
+}
+
+function renderEvents(events) {
+  const markup = events
+    .map(({ name, images }) => {
+      return `<li><h2>${name}</h2><img src='${images[0].url}' alt='${name}' width='200'></li>`;
+    })
+    .join("");
+  list.insertAdjacentHTML("beforeend", markup);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputValue = event.target.elements.query.value;
+  if (!inputValue.trim() || inputValue === queryToFetch) {
+    return;
+  }
+  queryToFetch = inputValue;
+  pageToFetch = 0;
+  list.innerHTML = "";
+  observer.unobserve(guard);
+  getEvents(queryToFetch, pageToFetch);
+  form.reset();
 }
